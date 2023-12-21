9665e33e97198e0d5d0865b7aebea0fe62b4716b
Akash Dabhane <akashdabhane10@gmail.com>
2023-12-22 01:05:08 +0530

    links in footer and bookinfo page layout change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
     return (
@@ -6,28 +7,28 @@ export default function Footer() {
             <div className="footer bg-gray-900 text-white text-left mt-10">
                 <div className="upperSection p-5 font-semibold text-lg flex flex-col md:text-base md:flex-row md:justify-evenly">
                     <div className="about flex flex-col">
-                        <div className="p-2 my-2 text-gray-500" href='/'>About</div>
-                        <a className="" href='/'>Contact Us</a>
-                        <a className="" href='/'>About Us</a>
-                        <a className="" href='/'>Careers</a>
-                        <a className="" href='/'>Wholesale</a>
-                        <a className="" href='/'>Corperate Information</a>
+                        <div className="p-2 my-2 text-gray-500">About</div>
+                        <Link className="" to='/contact-us'>Contact Us</Link>
+                        <Link className="" to='/about-app'>About Us</Link>
+                        <Link className="" to='/'>Careers</Link>
+                        <Link className="" to='/'>Wholesale</Link>
+                        <Link className="" to='/'>Corperate Information</Link>
                     </div>
                     <div className="help flex flex-col">
-                        <div  className="p-2 my-2 text-gray-500">Help</div>
-                        <a  className="" href='/'>Payment</a>
-                        <a  className="" href='/'>Shipping</a>
-                        <a  className="" href='/'>FAQ</a>
+                        <div className="p-2 my-2 text-gray-500">Help</div>
+                        <Link className="" to='/'>Payment</Link>
+                        <Link className="" to='/'>Shipping</Link>
+                        <Link className="" to='/'>FAQ</Link>
                     </div>
                     <div className="socialMedia flex flex-col">
-                        <div  className="p-2 my-2 text-gray-500">Social Media</div>
-                        <a  className="" href='https://www.instagram.com/' target='blank'>Instagram</a>
-                        <a  className="" href='https://www.facebook.com/' target='blank'>Facebook</a>
-                        <a  className="" href='https://twitter.com/' target='blank'>Twitter</a>
+                        <div className="p-2 my-2 text-gray-500">Social Media</div>
+                        <Link className="" to='https://www.instagram.com/' target='blank'>Instagram</Link>
+                        <Link className="" to='https://www.facebook.com/' target='blank'>Facebook</Link>
+                        <Link className="" to='https://twitter.com/' target='blank'>Twitter</Link>
                     </div>
                     <div className="mailUs flex flex-col">
                         <div className="p-2 my-2 text-gray-500">Email</div>
-                        <a href='mailto:example@gmail.com' className="">example@gmail.com</a>
+                        <Link to='mailto:example@gmail.com' className="">example@gmail.com</Link>
                     </div>
                 </div>
                 <div className="lowerSection py-2 text-center text-md border-t-2 border-gray-500">
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -35,35 +35,37 @@ export default function BookInfo() {
 
             <div className=" text-black space-y-2 w-full py-2 ">
                 <div className=" space-y-6  px-8 py-2 ">
-                    <div className="flex space-x-4">
-                        <img className='w-48 h-60' src={book.cover} alt="" />
-                        <div className="space-y-2">
-                            <h4 className='text-xl'>{book.name}</h4>
-                            <span className="athor text-gray-600">{book.author}</span>
+                    <div className="flex flex-col  md:flex-row space-x-0 md:space-x-4 space-y-3 md:space-y-0">
+                        <div className="flex space-x-4">
+                            <img className='w-48 h-60' src={book.cover} alt="" />
+                            <div className="space-y-2">
+                                <h4 className='text-xl'>{book.name}</h4>
+                                <span className="athor text-gray-600">{book.author}</span>
 
-                            <div className="flex space-x-4">
-                                <h3 className="price text-xl font-semibold">{book.price} </h3>
-                                <div className="relative ">
-                                    <span className="mrp text-xl font-semibold text-gray-600">{'20000'} </span>
-                                    <div className="relative bottom-3 h-[0.1rem] bg-gray-500"></div>
+                                <div className="flex space-x-4">
+                                    <h3 className="price text-xl font-semibold">{book.price} </h3>
+                                    <div className="relative ">
+                                        <span className="mrp text-xl font-semibold text-gray-600">{'20000'} </span>
+                                        <div className="relative bottom-3 h-[0.1rem] bg-gray-500"></div>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <span className="flex space-x-2">
-                                <div className='rating flex space-x-1 items-center bg-green-700 p-2 py-[1px] rounded text-white '>
-                                    <FaRegStar className='' />
-                                    <span>4.4</span>
+                                <span className="flex space-x-2">
+                                    <div className='rating flex space-x-1 items-center bg-green-700 p-2 py-[1px] rounded text-white '>
+                                        <FaRegStar className='' />
+                                        <span>4.4</span>
+                                    </div>
+                                    <span className="sells">({"111"})</span>
+                                </span>
+
+                                <div className="space-x-2">
+                                    <span>20 rating</span>
+                                    <span>&</span>
+                                    <span>4 Review</span>
                                 </div>
-                                <span className="sells">({"111"})</span>
-                            </span>
-
-                            <div className="space-x-2">
-                                <span>20 rating</span>
-                                <span>&</span>
-                                <span>4 Review</span>
                             </div>
                         </div>
-                        <div className="buy space-x-4 px-11">
+                        <div className="buy flex justify-center items-start space-x-4 sm:px-2 md:px-11">
                             <button className='bg-blue-600 p-3 px-10 text-white '>Buy Book</button>
                             <button className='bg-blue-600 p-3 px-10 text-white '>Renk Book</button>
                         </div>
