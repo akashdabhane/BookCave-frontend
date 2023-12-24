de1eec17cbdcf0d00304852d929f1a6260eb8596
Akash Dabhane <akashdabhane10@gmail.com>
2023-12-24 16:17:37 +0530

    contact us / send email functionality created

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import axios from 'axios';
+import { baseUrl } from '../utils/baseUrl';
 
 
 export default function ContactUs() {
+    const [emailData, setEmailData] = useState({
+        from: "",
+        subject: "",
+        message: "",
+        fullname: "",
+        phone: ""
+    });
+
+    const handleOnChange = (event) => {
+        const { name, value } = event.target;
+        setEmailData({
+            ...emailData,
+            [name]: value
+        })
+    }
+
+    const handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(emailData);
+
+        try {
+            axios.post(`${baseUrl}/api/contact-us`, emailData)
+            .then(data => {
+                console.log('data', data); 
+            })
+            .catch(error => {
+                console.log('error', error); 
+            })
+        } catch (error) {
+            console.log(error); 
+        }
+    }
+
+
+
     return (
         <div className='md:mx-[20%] text-black bg-white h-[100vh]'>
             <Navbar />
@@ -11,31 +48,36 @@ export default function ContactUs() {
             <h2 className='font-bold text-2xl m-7 text-center'>Let's start conversition</h2>
 
             <div className="flex justify-around">
-                <form action="submit" className="">
+                <form className="">
                     <span>note: all fields are required*</span>
 
                     <main className='space-y-2 '>
                         <div className="">
-                            <label htmlFor="firstname" className='block text-sm'>First Name</label>
-                            <input type="text" className='px-4 p-2 border w-80 rounded outline-none' name="firstname" id="firstname" />
-                        </div>
-
-                        <div className="">
-                            <label htmlFor="lastname" className='block text-sm'>Last Name</label>
-                            <input type="text" className='px-4 p-2 border w-80 rounded outline-none' name="lastname" id="lastname" />
+                            <label htmlFor="fullname" className='block text-sm'>Full Name</label>
+                            <input type="text" className='px-4 p-2 border w-80 rounded outline-none' name="fullname" id="fullname" onChange={handleOnChange} />
                         </div>
 
                         <div className="phone">
                             <label htmlFor="phone" className='block text-sm'>phone</label>
-                            <input type="tel" className='px-4 p-2 border w-80 rounded outline-none' name="phone" id="phone" placeholder="(123)456-7890" />
+                            <input type="tel" className='px-4 p-2 border w-80 rounded outline-none' name="phone" id="phone" placeholder="(123)456-7890" onChange={handleOnChange} />
+                        </div>
+
+                        <div className="email">
+                            <label htmlFor="email" className='block text-sm'>email</label>
+                            <input type="tel" className='px-4 p-2 border w-80 rounded outline-none' name="from" id="email" placeholder="example@gmail.com" onChange={handleOnChange} />
+                        </div>
+
+                        <div className="">
+                            <label htmlFor="subject" className='block text-sm'>Subject</label>
+                            <input type="text" className='px-4 p-2 border w-80 rounded outline-none' name="subject" id="subject" onChange={handleOnChange} />
                         </div>
 
                         <div className="">
                             <label htmlFor="message" className='block text-sm'>message</label>
-                            <textarea name="message" className='px-4 p-2 border w-80 rounded outline-none' id="message" cols="30" rows="8"></textarea>
+                            <textarea name="message" className='px-4 p-2 border w-80 rounded outline-none' id="message" cols="30" rows="8" onChange={handleOnChange}></textarea>
                         </div>
 
-                        <input type="submit" value="Submit" className=' w-80 bg-blue-600 rounded p-2 text-white font-semibold' />
+                        <button onClick={handleOnSubmit} className='w-80 bg-blue-600 rounded p-2 text-white font-semibold' >Submit</button>
                     </main>
                 </form>
 
